452be5f15a983c401bd88f279d984d42e306bd66
Joel Tanig <joeltanig77@gmail.com>
2021-03-15 19:34:56 -0700

    Completed search for wiki page using BFS
    Found the star wars wiki page and returned the fastest path to star wars by only clicking links within articles. Last thing to modify is the terminal output

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from bs4 import BeautifulSoup  # Parsing HTML
 import re  # Regular expressions
 import requests  # Fetching pages
-from collections import deque  # Free queue structure
-
-
+from collections import deque  # Queue structure
 
 
 wikiFormat = "https://en.wikipedia.org"
 
+class Path:
+    def __init__(self,url,list):
+        self.url = url
+        self.list = list
+
+
+
+
 def get_random_page_url():
-    r = requests.get('https://en.wikipedia.org/wiki/George_Lucas')
+    r = requests.get('https://en.wikipedia.org/wiki/Manga')
     return r.url
 #https://en.wikipedia.org/wiki/Special:Random'
+#https://en.wikipedia.org/wiki/George_Lucas
+
 
 def internal_not_special(href):
     if href:
@@ -21,66 +29,68 @@ def internal_not_special(href):
                     return True
     return False
 
-#For every link call it again
-
 
 def findValidLinks(url):
     url = requests.get(url)
     beautifulSoup = BeautifulSoup(url.text, 'html.parser')
     pageTitle = beautifulSoup.find("h1", id="firstHeading").string
-    print(pageTitle)
     mainContent = beautifulSoup.find(id="bodyContent")
     # Let us now look for links
     linkLists = []
-    finalLink = ""
     validLinks = mainContent.find_all('a', href=internal_not_special)
     for i in range(len(validLinks)):
         tempLink = validLinks[i].get("href")
         finalLink = wikiFormat + tempLink
-
-        if finalLink in linkLists:
-            continue
+        #print(finalLink)
         linkLists.append(finalLink)
-
-   # for i in range(len(linkLists)):
-       # print(linkLists[i])
 
     return linkLists
 
 
-
-
-
 def bfs(vertex):
     q = deque()
-    path = deque()
-    q.appendleft(vertex)
-    discovered = []
+    path = Path(vertex, [])
+    path.list.append(vertex)
+    q.appendleft(path)
+    discovered = [vertex]
     while q:
-        vertex = q.pop()
-        if vertex == 'https://en.wikipedia.org/wiki/Star_Wars':
-            return "FOUNDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"
-        linkLists = findValidLinks(vertex)
-        for i in range(len(linkLists)):
-            if linkLists[i] not in discovered:
-                print(linkLists[i])
-                if (linkLists[i] == 'https://en.wikipedia.org/wiki/Star_Wars'):
-                    return
-                discovered.append(linkLists[i])
-                q.appendleft(linkLists[i])
+        popper = q.pop()
+        print("Searching.... " + popper.url)
+        url = popper.url
+        #print(vertex.list)
+        linkLists = findValidLinks(url)
+        #print(len(popper.list))
+        #Check that we only go for 6 depth here
+        if (len(popper.list) < 6):
+            #print(len(linkLists))
+            for i in range(len(linkLists)):
+                #print(vertex.url)
+                if linkLists[i] not in discovered:
+                    #print(linkLists[i])
+                    #print(linkLists[i])
+                    pathObj = Path(linkLists[i], None)
+                    pathObj.list = popper.list + [linkLists[i]]
+                    #print(pathObj.list)
+                    if (pathObj.url == 'https://en.wikipedia.org/wiki/Star_Wars'):
+                        return printPath(pathObj.list)
 
+                    discovered.append(pathObj.url)
+                    q.appendleft(pathObj)
+
+    return didNotFinish()
 
 
 def printPath(path):
-    print(f"The final link is {path}")
-    exit(0)
+    print(f"The final Path is {path}")
+
+
+def didNotFinish():
+    print("Could not find path to wiki page in 6 steps")
 
 
 def main():
     randURL= get_random_page_url()
-    #print(dictLinks)
     bfs(randURL)
-    return None
 
 
 # Press the green button in the gutter to run the script.
