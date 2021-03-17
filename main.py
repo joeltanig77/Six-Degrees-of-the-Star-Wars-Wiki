4ae1cde18e91bfcd2c22cc7e9b63077caf6a2446
Joel Tanig <joeltanig77@gmail.com>
2021-03-17 08:48:18 -0700

    Cleaned up the output
    Added titles next to the link path for the output when we find the target wiki page

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
-from bs4 import BeautifulSoup  # Parsing HTML
-import re  # Regular expressions
-import requests  # Fetching pages
-from collections import deque  # Queue structure
+from bs4 import BeautifulSoup   # Parsing HTML
+import re                       # Regular expressions
+import requests                 # Fetching pages
+from collections import deque   # Queue structure
 
 
-wikiFormat = "https://en.wikipedia.org"
-
+# Stores the path for each url
 class Path:
-    def __init__(self,url,list):
+    def __init__(self, url, list):
         self.url = url
         self.list = list
 
 
+# Store start and end urls
+class Storage:
+    def __init__(self, url="", endURL=""):
+        self.url = url
+        self.endURL = endURL
+
+    def setStartUrl(self, url):
+        self.url = url
+
+    def setEndURL(self, endURL):
+        self.endURL = endURL
+
+    def getStartUrl(self):
+        return self.url
+
+    def getENDURL(self):
+        return self.endURL
+
+
+title = ""
+store = Storage()
+wikiFormat = "https://en.wikipedia.org"
 
 
 def get_random_page_url():
-    r = requests.get('https://en.wikipedia.org/wiki/Manga')
+    r = requests.get('https://en.wikipedia.org/wiki/Special:Random')
     return r.url
-#https://en.wikipedia.org/wiki/Special:Random'
-#https://en.wikipedia.org/wiki/George_Lucas
 
 
 def internal_not_special(href):
+    # Check to make sure its a wiki link
     if href:
         if re.compile('^/wiki/').search(href):
             if not re.compile('/\w+:').search(href):
@@ -31,9 +51,9 @@ def internal_not_special(href):
 
 
 def findValidLinks(url):
+    # Parse the html from the wiki page
     url = requests.get(url)
     beautifulSoup = BeautifulSoup(url.text, 'html.parser')
-    pageTitle = beautifulSoup.find("h1", id="firstHeading").string
     mainContent = beautifulSoup.find(id="bodyContent")
     # Let us now look for links
     linkLists = []
@@ -41,58 +61,62 @@ def findValidLinks(url):
     for i in range(len(validLinks)):
         tempLink = validLinks[i].get("href")
         finalLink = wikiFormat + tempLink
-        #print(finalLink)
         linkLists.append(finalLink)
 
     return linkLists
 
 
+# Breadth first search the wiki pages
 def bfs(vertex):
     q = deque()
     path = Path(vertex, [])
     path.list.append(vertex)
     q.appendleft(path)
     discovered = [vertex]
+    print(f"Commencing Search for the {store.getENDURL()} wiki page\n")
     while q:
         popper = q.pop()
         print("Searching.... " + popper.url)
         url = popper.url
-        #print(vertex.list)
         linkLists = findValidLinks(url)
-        #print(len(popper.list))
-        #Check that we only go for 6 depth here
         if (len(popper.list) < 6):
-            #print(len(linkLists))
             for i in range(len(linkLists)):
-                #print(vertex.url)
                 if linkLists[i] not in discovered:
-                    #print(linkLists[i])
-                    #print(linkLists[i])
+                    # Save the path
                     pathObj = Path(linkLists[i], None)
                     pathObj.list = popper.list + [linkLists[i]]
-                    #print(pathObj.list)
-                    if (pathObj.url == 'https://en.wikipedia.org/wiki/Star_Wars'):
+                    # If found, return and give us the path to our target
+                    if (pathObj.url == store.getENDURL()):
                         return printPath(pathObj.list)
 
                     discovered.append(pathObj.url)
                     q.appendleft(pathObj)
 
-    return didNotFinish()
+    return didNotFind()
 
 
 def printPath(path):
-    print(f"The final Path is {path}")
+    print("\nFOUND!! Your fastest path to Star Wars is....")
+    tab = ""
+    for i in range(len(path)):
+        url = requests.get(path[i])
+        beautifulSoup = BeautifulSoup(url.text, 'html.parser')
+        pageTitle = beautifulSoup.find("h1", id="firstHeading").string
+        print(f"{tab}{pageTitle} ({path[i]})")
+        tab += '\t'
 
 
-def didNotFinish():
-    print("Could not find path to wiki page in 6 steps")
+def didNotFind():
+    print(f"Could not find path from {store.getStartUrl()} to {store.getENDURL()} in 6 steps. Please try again")
 
 
 def main():
-    randURL= get_random_page_url()
+    randURL = get_random_page_url()
+    endURL = "https://en.wikipedia.org/wiki/Star_Wars"
+    store.setEndURL(endURL)
+    store.setStartUrl(randURL)
     bfs(randURL)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
